d370284bcb4ad28e4f4c28e9ecbd2eb2befd9d4c
rookieCookies <rookieCookies@users.noreply.github.com>
2023-05-30 13:30:32 +0000

    deploy: d5697c9ed6dfd046c7be8f107ee4057013c08406

---
+++ deps/github_test-d12e897f49c3b786.d
@@ -0,0 +1,5 @@
+/home/runner/work/github_actions_test/github_actions_test/target/release/deps/github_test-d12e897f49c3b786: src/main.rs
+
+/home/runner/work/github_actions_test/github_actions_test/target/release/deps/github_test-d12e897f49c3b786.d: src/main.rs
+
+src/main.rs:
---
+++ github_test.d
@@ -0,0 +1,1 @@
+/home/runner/work/github_actions_test/github_actions_test/target/release/github_test: /home/runner/work/github_actions_test/github_actions_test/src/main.rs
